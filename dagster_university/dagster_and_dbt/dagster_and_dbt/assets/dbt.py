6aa949c086b8e6045ded2080186ea7e38faf91ca
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2025-05-22 16:18:31 +1000

    import DBT sources to Dagster

--- dagster_university/dagster_and_dbt/dagster_and_dbt/assets/dbt.py
+++ dagster_university/dagster_and_dbt/dagster_and_dbt/assets/dbt.py
@@ -1,8 +1,23 @@
+from typing import Any, Mapping
+
 import dagster as dg
 from dagster_and_dbt.project import dbt_project
-from dagster_dbt import DbtCliResource, dbt_assets
+from dagster_dbt import DagsterDbtTranslator, DbtCliResource, dbt_assets
 
 
-@dbt_assets(manifest=dbt_project.manifest_path)
+class CustomizedDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_asset_key(self, dbt_resource_props: Mapping[str, Any]) -> dg.AssetKey:
+        resource_type = dbt_resource_props["resource_type"]
+        name = dbt_resource_props["name"]
+        if resource_type == "source":
+            return dg.AssetKey(f"taxi_{name}")
+        else:
+            return super().get_asset_key(dbt_resource_props)
+
+
+@dbt_assets(
+    manifest=dbt_project.manifest_path,
+    dagster_dbt_translator=CustomizedDagsterDbtTranslator(),
+)
 def dbt_analytics(context: dg.AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
